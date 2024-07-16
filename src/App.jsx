ae6afd6fc3e4d09a52242a38f4a4c1113e680ed5
Karunakar <narlakondakarunakar2k18@gmail.com>
2024-07-16 22:20:18 +0530

    added warning

--- src/App.jsx
+++ src/App.jsx
@@ -10,37 +10,42 @@ let count=0;
 function App() {
   const [Todo,setTodo]=useState("");
   const [Todos,setTodos]=useState([]);
-  
+  const [Isempty,setIsempty]=useState("");
   return (
     <>
+    <div className='w-1/2 border-4 border-indigo-500/75'>
       <div>
+        <p>To -Do list</p>
+      </div>
+      <div className='flex flex-row justify-between '>
         
-        <input className='' type='text' placeholder='Enter Todo task' value={Todo} onChange={(e)=>{
+        <input className='w-1/4' type='text' placeholder='Enter Todo task' value={Todo} onChange={(e)=>{
           setTodo(e.target.value);
-          console.log(Todo);
         }}/>
         <button className='bg-[#1da1f2]' onClick={()=>{
         if(Todo===""){
-          return;
+          setIsempty("Please enter a todo to get started")
+          return ;
         }
         let arr=[...Todos,{"id":count,"task":Todo}];
         count++;
         setTodos(arr);
-        console.log(count)
         setTodo("");
-
+        setIsempty("")
         }}>Add Todo</button>
+
       </div>
-      <div>
+      <div >
         {
           Todos.map((i)=>{
-            return <h1  onClick={()=>{
+            return <div className='flex flex-row justify-between ' key={i.id}><p>{i.task}</p><button className='bg-[#1e293b] ' onClick={()=>{
               setTodos(del(i.id,Todos))
-            }}>{i.task}</h1>
+            }}>Done</button></div>
           })
         }
+        <p className='text-red-600'>{Isempty}</p>
       </div>
-      
+      </div>
     </>
   )
 }
